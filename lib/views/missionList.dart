78393d1e5a5e5fcacaa6f833e2bf3a8447832860
Joshua Arus <joshua.arus@gmail.com>
2021-06-15 20:14:39 +0200

    Transformation de la page MissionList en ExpansionPanelList

--- lib/views/missionList.dart
+++ lib/views/missionList.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:the_crew_companion/constant.dart';
+import 'package:the_crew_companion/views/components/playGameComponents.dart';
 
 import '../controller.dart';
 
@@ -12,15 +14,52 @@ class MissionList extends StatefulWidget {
 }
 
 class _MissionListState extends State<MissionList> {
+
+  List<bool> isOpen = [];
+
+  void _expand(int index, bool isExpanded) {
+    setState(() {
+      isOpen[index] = !isExpanded;
+    });    
+  }
+
   @override
   Widget build(BuildContext context) {
+    if (isOpen.length == 0)
+      isOpen = widget.controller.missions.map((e) => false).toList();
     return Scaffold(
       appBar: AppBar(
         title: Text("Liste des missions"),
         centerTitle: true,
       ),
-      body: Container(
-        child: Text("")
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(defaultPadding),
+          child: ExpansionPanelList(
+            expansionCallback: _expand,
+            children: widget.controller.missions.map((mission) {
+              return ExpansionPanel(
+                canTapOnHeader: true,
+                headerBuilder: (BuildContext context, bool isOpen) {
+                  return ListTile(
+                    title: Text(mission.title)
+                  );
+                },
+                body: ListTile(
+                  title: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(mission.description),
+                      Padding(padding: EdgeInsets.all(defaultPadding)),
+                      MissionAims(currentMission: mission)
+                    ],
+                  ),
+                ),
+                isExpanded: isOpen[mission.id]
+              );
+            }).toList()
+          )
+        )
       )
     );
   }
