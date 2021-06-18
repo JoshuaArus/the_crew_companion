cbd979eefbc293333ee52175efed9ed7b4ecd2db
Joshua Arus <joshua.arus@gmail.com>
2021-06-18 17:11:06 +0200

    Fix splashscreen

--- lib/views/splashScreen.dart
+++ lib/views/splashScreen.dart
@@ -7,9 +7,11 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: CircularProgressIndicator(
-        color: primaryColor,
-        strokeWidth: 5,
+      child : Center(
+        child: CircularProgressIndicator(
+          color: primaryColor,
+          strokeWidth: 5,
+        )
       )
     );
   }
