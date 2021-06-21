ebfb55f48dc8ba94851050b3e8ec00c4aa18e21a
Joshua Arus <joshua.arus@gmail.com>
2021-06-21 23:43:49 +0200

    Ajout de package_info_plus dans le controller pour récupérer les informations de l'application comme le nom et le numéro de version

--- lib/controller.dart
+++ lib/controller.dart
@@ -1,14 +1,26 @@
 import 'dart:convert';
 
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-import 'constant.dart';
 import 'entities/mission.dart';
 import 'entities/team.dart';
 import 'story.dart';
 
 class Controller {
   
+  final storageKey = "com.joshuaarus.the_crew_companion";
+
+  late PackageInfo infos;
+
+  String get developper => "Joshua Arus";
+  String get appName => infos.appName;
+  String get appVersion => infos.version;
+
+  Future<void> init () async {
+    infos = await PackageInfo.fromPlatform();
+  }
+
   List<Team> teams = [];
   final List<Mission> missions = Story.missions;
 
@@ -25,5 +37,5 @@ class Controller {
       this.teams = (jsonDecode(data) as List<dynamic>).map((t) => Team.fromJson(t)).toList();
     }
     return true;
-  }
+  }  
 }
