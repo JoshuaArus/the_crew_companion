526f2bf67dbbbe58bd1a3a8657612e0260c9a96f
Joshua Arus <joshua.arus@gmail.com>
2021-06-21 23:45:22 +0200

    Ajout de la page HomeScreen au lancement de l'application qui remplace teamList. Réorganisation du workflow des pages. Déplacement de

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 
-const storageKey = "com.joshuaarus.the_crew_companion";
-const appTitle = 'The Crew Companion';
-
 const primaryColor = Color(0xFF2697FF);
 const secondaryColor = Color(0xFF2A2D3E);
 const bgColor = Color(0xFF212332);
---
+++ lib/views/components/homeScreenButton.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:the_crew_companion/constant.dart';
+
+class HomeScreenButton extends StatefulWidget {
+  const HomeScreenButton({ Key? key, required this.text, required this.onPressed, this.disabled = false}) : super(key: key);
+
+  final String text;
+  final Function onPressed;
+  final bool disabled;
+
+  @override
+  _HomeScreenButtonState createState() => _HomeScreenButtonState();
+}
+
+class _HomeScreenButtonState extends State<HomeScreenButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(left: defaultPadding*2, right: defaultPadding*2),
+      width: double.infinity,
+      height: 50,
+      child : OutlinedButton(
+        onPressed: () { if (widget.disabled) return; widget.onPressed(); },
+        child: Text(
+          widget.text.toUpperCase(),
+          style: Theme.of(context).textTheme.headline5!.copyWith(
+            fontWeight: FontWeight.bold,
+            color: Colors.white.withOpacity(widget.disabled ? 0.2 : 1)
+          ),
+          overflow: TextOverflow.ellipsis,
+        ),
+        style: OutlinedButton.styleFrom(
+          backgroundColor: secondaryColor.withOpacity(0.5),
+          side: BorderSide(
+            width: 2,
+            color: primaryColor.withOpacity(0.70),
+          ),
+          shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(30)))
+        ),
+      )
+    );
+  }
+}
