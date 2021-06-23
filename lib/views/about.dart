bf875351469992d7caff33421346353608e538ca
Joshua Arus <joshua.arus@gmail.com>
2021-06-23 15:59:21 +0200

    Rework page about pour inclure le logo et modifier le bloc de texte

--- lib/views/about.dart
+++ lib/views/about.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:the_crew_companion/constant.dart';
+import 'package:the_crew_companion/views/components/delayedAnimation.dart';
+import 'package:the_crew_companion/views/components/jumpingHomeScreenTitle.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 import '../controller.dart';
@@ -26,55 +28,76 @@ class _AboutState extends State<About> {
         centerTitle: true,
       ),
       body: Container(
-        width: double.infinity,
         decoration: BoxDecoration(
           image: DecorationImage(
-              image: AssetImage("assets/images/homeScreenBackground.png"),
-              fit: BoxFit.cover),
+            image: AssetImage("assets/images/homeScreenBackground.png"),
+            fit: BoxFit.fill,
+          ),
         ),
         child: Container(
-          padding: EdgeInsets.only(top: 270),
-          child: Container(
-            padding: EdgeInsets.all(defaultPadding * 2),
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.white, width: 2),
-              borderRadius: BorderRadius.all(Radius.circular(8)),
-              color: secondaryColor.withOpacity(0.7),
-              shape: BoxShape.rectangle,
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                MarkdownBody(
-                  data:
-                      "Application mobile non officielle permettant de remplacer le carnet du jeu [The Crew](https://boardgamegeek.com/boardgame/284083/crew-quest-planet-nine) et de suivre les parties réalisées.",
-                  styleSheet: MarkdownStyleSheet(
-                    textAlign: WrapAlignment.spaceEvenly,
-                    p: TextStyle(
-                      height: 2,
-                    ),
+          child: Column(
+            children: [
+              Expanded(
+                flex: 1,
+                child: Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 30,
                   ),
-                  onTapLink: (text, url, title) {
-                    launch(url!);
-                  },
-                ),
-                Text("Développé par " + developper),
-                MarkdownBody(
-                  data:
-                      "Suivre le projet sur [github](https://github.com/JoshuaArus/the_crew_companion/)",
-                  styleSheet: MarkdownStyleSheet(
-                    textAlign: WrapAlignment.spaceEvenly,
-                    p: TextStyle(
-                      height: 2,
-                    ),
+                  child: DelayedAnimation(
+                    delay: 1000,
+                    child: JumpingHomeScreenTitle(),
                   ),
-                  onTapLink: (text, url, title) {
-                    launch(url!);
-                  },
                 ),
-                Text("Version " + version),
-              ],
-            ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Container(
+                  padding: EdgeInsets.all(defaultPadding * 2),
+                  margin: EdgeInsets.all(defaultPadding),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.white, width: 2),
+                    borderRadius: BorderRadius.all(Radius.circular(8)),
+                    color: secondaryColor.withOpacity(0.90),
+                    shape: BoxShape.rectangle,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      MarkdownBody(
+                        data:
+                            "Application mobile non officielle permettant de remplacer le carnet du jeu [The Crew](https://boardgamegeek.com/boardgame/284083/crew-quest-planet-nine) et de suivre les parties réalisées.",
+                        styleSheet: MarkdownStyleSheet(
+                          textAlign: WrapAlignment.spaceEvenly,
+                          p: TextStyle(
+                            height: 2,
+                          ),
+                        ),
+                        onTapLink: (text, url, title) {
+                          launch(url!);
+                        },
+                      ),
+                      Text("Développé par " + developper),
+                      MarkdownBody(
+                        data:
+                            "Suivre le projet sur [github](https://github.com/JoshuaArus/the_crew_companion/)",
+                        styleSheet: MarkdownStyleSheet(
+                          textAlign: WrapAlignment.spaceEvenly,
+                          p: TextStyle(
+                            height: 2,
+                          ),
+                        ),
+                        onTapLink: (text, url, title) {
+                          launch(url!);
+                        },
+                      ),
+                      Text("Version " + version),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
