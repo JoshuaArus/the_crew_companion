0ab552d547a48acdac6732ff4d434c5d94709c77
Joshua Arus <joshua.arus@gmail.com>
2021-06-27 00:26:08 +0200

    Fix import

--- lib/entities/aimOption.dart
+++ lib/entities/aimOption.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:the_crew_companion/CustomThemes.dart';
+import 'package:the_crew_companion/customThemes.dart';
 
 enum AimOption {
   One,
