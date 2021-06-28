29e6b4b200e240dbd0439f8fe45bc8186629ab77
Joshua Arus <joshua.arus@gmail.com>
2021-06-28 15:49:24 +0200

    extraction de la liste des contributeurs pour être stockée directement dans la page about

--- lib/views/about.dart
+++ lib/views/about.dart
@@ -17,6 +17,11 @@ class About extends StatefulWidget {
 }
 
 class _AboutState extends State<About> {
+  List<String> contributors = [
+    "[Joshua Arus](https://github.com/JoshuaArus)",
+    "[Maxime Rauch](https://github.com/schnapse)",
+  ];
+
   @override
   Widget build(BuildContext context) {
     String version = widget.controller.appVersion;
@@ -75,21 +80,11 @@ class _AboutState extends State<About> {
                             data:
                                 AppLocalizations.translate('aboutPresentation'),
                           ),
-                          MarkdownBody(
+                          CustomMarkdownBody(
                             data:
                                 AppLocalizations.translate('aboutDevelopedBy') +
-                                    " " +
-                                    AppLocalizations.translate(
-                                        'applicationDeveloper'),
-                            styleSheet: MarkdownStyleSheet(
-                              textAlign: WrapAlignment.spaceEvenly,
-                              p: TextStyle(
-                                height: 2,
-                              ),
-                            ),
-                            onTapLink: (text, url, title) {
-                              launch(url!);
-                            },
+                                    " : " +
+                                    contributors.join(" - "),
                           ),
                           CustomMarkdownBody(
                             data: AppLocalizations.translate(
