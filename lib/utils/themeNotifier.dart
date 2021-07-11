518181306e3a873329ea7bfa268f5bdc916f26a8
Joshua Arus <joshua.arus@gmail.com>
2021-07-11 08:20:10 +0200

    Passage du paramètre "theme" de ThemeNotifier en obligatoire pour éviter des désync entre le notifier et le reste de l'app

--- lib/utils/themeNotifier.dart
+++ lib/utils/themeNotifier.dart
@@ -4,7 +4,7 @@ import 'package:the_crew_companion/utils/customThemes.dart';
 class ThemeNotifier extends ChangeNotifier {
   CustomThemes _theme;
 
-  ThemeNotifier({CustomThemes theme = CustomThemes.Dark}) : this._theme = theme;
+  ThemeNotifier({required CustomThemes theme}) : this._theme = theme;
 
   CustomThemes getTheme() {
     return _theme.copy; //copy to avoid ref override
